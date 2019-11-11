99d80224093caa12fd1ea9255c8bebe5c2ff5c8e
Travis CI <travis@travis-ci.org>
2019-11-11 15:45:05 +0000

    Travis build: 50 | Update SHA256 for formula vsh-php70 and OS catalina

--- Formula/vsh-php70.rb
+++ Formula/vsh-php70.rb
@@ -6,7 +6,7 @@ class VshPhp70 < Formula
 
   bottle do
     root_url "https://dl.bintray.com/valet-sh/homebrew-core"
-    sha256 "c09396d2fc055d74f0f915c475ded38e8f7afac70ca5cc05546629e42fc654f1" => :catalina
+    sha256 "37c75091ef136ec1c5837a2eee189c1075c72f0acbfc6cadc9fc6993cdcb8084" => :catalina
     sha256 "1278858c1b7ea78072aa855d2a2de19d6185d000b583787632fb364a4c37157a" => :mojave
     rebuild 1
   end
