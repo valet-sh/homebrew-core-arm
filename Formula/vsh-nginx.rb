9881c19a645b1f051a96e9276366de6f57655254
CI <ci@valet.sh>
2019-12-13 14:38:04 +0000

    Update bottle information in formulas

--- Formula/vsh-nginx.rb
+++ Formula/vsh-nginx.rb
@@ -9,7 +9,7 @@ class VshNginx < Formula
 
   bottle do
     root_url "https://dl.bintray.com/valet-sh/homebrew-core"
-    sha256 "d9faab59f0e96679d8bc43b0d53ee0dba9dc68859cbba7190e7b296057cef394" => :catalina
+    sha256 "a6eeafea56bff65ddc26344b57b92f90e1a67c430c2ae88a8e5204683b280e3a" => :catalina
     sha256 "6b6cf829e3e9c6c48153786268e5571ac71689e44f88aa02a12d94c76d02afe4" => :mojave
   end
 
