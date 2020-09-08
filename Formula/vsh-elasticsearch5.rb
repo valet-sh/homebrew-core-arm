dd19ae99a94a8caa82d716cad0fa840d37c38911
Philipp Dittert <p.dittert@techdivision.com>
2020-09-08 13:54:08 +0200

    add revision tag

--- Formula/vsh-elasticsearch5.rb
+++ Formula/vsh-elasticsearch5.rb
@@ -4,10 +4,11 @@ class VshElasticsearch5 < Formula
   url "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.6.16.tar.gz"
   sha256 "6b035a59337d571ab70cea72cc55225c027ad142fbb07fd8984e54261657c77f"
   license "Apache-2.0"
+  revision 1
 
   bottle do
     root_url "https://github.com/valet-sh/homebrew-core/releases/download/bottles"
-    rebuild 1
+    rebuild 2
     sha256 "b09ebc78a3d7036994d15060cf89cd7ecb90fd11341357f8c616e587c2b9e930" => :catalina
     sha256 "adfe570bd8f9828cb87a2b48a30d2f35c79135fee2efaa942d090bb4dfc62bfb" => :mojave
 
