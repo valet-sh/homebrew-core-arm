3d84f30bbaf00744fd20977fed9e0fce30086450
CI <ci@valet.sh>
2020-09-08 11:48:31 +0000

    [skip ci] Update bottle information in formulae ***NO_CI***

--- Formula/vsh-elasticsearch5.rb
+++ Formula/vsh-elasticsearch5.rb
@@ -8,7 +8,7 @@ class VshElasticsearch5 < Formula
   bottle do
     root_url "https://github.com/valet-sh/homebrew-core/releases/download/bottles"
     rebuild 1
-    sha256 "b09ebc78a3d7036994d15060cf89cd7ecb90fd11341357f8c616e587c2b9e930" => :catalina
+    sha256 "05197edae56a4e5b037174a5a5488e07b5a9b501145c93cc4a33f491261727cd" => :catalina
     sha256 "adfe570bd8f9828cb87a2b48a30d2f35c79135fee2efaa942d090bb4dfc62bfb" => :mojave
 
   end
