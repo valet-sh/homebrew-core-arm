1d4013a767a551baa5221e3897ca83eecfe2cb05
Philipp Dittert <p.dittert@techdivision.com>
2020-09-29 17:10:00 +0200

    trigger new build for elasticsearch7

--- Formula/vsh-elasticsearch7.rb
+++ Formula/vsh-elasticsearch7.rb
@@ -3,7 +3,7 @@ class VshElasticsearch7 < Formula
   homepage "https://www.elastic.co/products/elasticsearch"
   url "https://github.com/elastic/elasticsearch/archive/v7.8.1.tar.gz"
   sha256 "e222d4165fb4145222491e1ed33dad15acc7b56334ca6589202e2ee761900c78"
-  revision 4
+  revision 5
   license "Apache-2.0"
 
   bottle do
