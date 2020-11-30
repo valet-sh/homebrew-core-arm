c1ebd9f50a6befb579e6711d90fff55654586156
Johann Zelger <j.zelger@techdivision.com>
2020-11-30 21:32:44 +0100

    Removed mojave bottle sha

--- Formula/vsh-php70.rb
+++ Formula/vsh-php70.rb
@@ -8,7 +8,6 @@ class VshPhp70 < Formula
   bottle do
     root_url "https://github.com/valet-sh/homebrew-core/releases/download/bottles"
     sha256 "189d2d11cd9b426f4dc65b21e1e044215ad3b82f8e897e79043c85fb74f851e1" => :catalina
-    sha256 "c7d93b550751ae5f0347f0227dba97b7ef2003922fdffdf55d287877040260d8" => :mojave
   end
 
   depends_on "pkg-config" => :build
