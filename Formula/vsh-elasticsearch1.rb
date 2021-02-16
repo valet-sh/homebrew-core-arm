f5d43a5c6dbdeb52593bbbc10e58b56eb76c3015
Philipp Dittert <p.dittert@techdivision.com>
2021-02-16 13:29:19 +0100

    remove invalid big_sur bottle definition from es1 formulae

--- Formula/vsh-elasticsearch1.rb
+++ Formula/vsh-elasticsearch1.rb
@@ -4,12 +4,11 @@ class VshElasticsearch1 < Formula
   url "https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.6.tar.gz"
   sha256 "78affc30353730ec245dad1f17de242a4ad12cf808eaa87dd878e1ca10ed77df"
   license "Apache-2.0"
-  revision 9
+  revision 10
 
   bottle do
     root_url "https://github.com/valet-sh/homebrew-core/releases/download/bottles"
     sha256 "86c5d12348212fbb0ae35a8dda2ac75103626d89dd16785f5c01f69d13ab17aa" => :catalina
-    sha256 "86c5d12348212fbb0ae35a8dda2ac75103626d89dd16785f5c01f69d13ab17xx" => :big_sur
   end
 
   depends_on "openjdk@8"
