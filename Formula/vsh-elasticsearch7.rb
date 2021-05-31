284f4e9f2ffab64346c37f4a87b99f2cb8e9e26a
Philipp Dittert <p.dittert@techdivision.com>
2021-05-31 13:05:42 +0200

    bump es7 version to 7.10.2

--- Formula/vsh-elasticsearch7.rb
+++ Formula/vsh-elasticsearch7.rb
@@ -1,9 +1,9 @@
 class VshElasticsearch7 < Formula
   desc "Distributed search & analytics engine"
   homepage "https://www.elastic.co/products/elasticsearch"
-  url "https://github.com/elastic/elasticsearch/archive/v7.9.3.tar.gz"
-  revision 3
-  sha256 "b378ebfcfcfa8c75007b7a6a06d2591d784902e08dc833e004007146fa5733cc"
+  url "https://github.com/elastic/elasticsearch/archive/v7.10.2.tar.gz"
+  sha256 "bdb7811882a0d9436ac202a947061b565aa71983c72e1c191e7373119a1cdd1c"
+  revision 1
   license "Apache-2.0"
 
   bottle do
