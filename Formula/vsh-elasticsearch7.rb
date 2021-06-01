16295216390636a2ab91363c6ebfff1ae14a300f
Philipp Dittert <p.dittert@techdivision.com>
2021-06-01 10:56:05 +0200

    change sha256 hash of source package in es7 Formula

--- Formula/vsh-elasticsearch7.rb
+++ Formula/vsh-elasticsearch7.rb
@@ -8,7 +8,7 @@ class VshElasticsearch7 < Formula
 
   bottle do
     root_url "https://github.com/valet-sh/homebrew-core/releases/download/bottles"
-    sha256 catalina: "e75c7c3b9c63af9607228d671c700bd3c687bbec61bbd01485a51638d00f5017"
+    sha256 cellar: :any_skip_relocation, catalina: "e75c7c3b9c63af9607228d671c700bd3c687bbec61bbd01485a51638d00f5017"
   end
 
   depends_on "gradle@6" => :build
