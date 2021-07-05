6482f6d06b61ea2fe2727664b49ff1b7dfdae641
zelgerj <zelgerj@users.noreply.github.com>
2021-07-05 13:01:21 +0000

    Trigger bottle rebuild

--- Formula/vsh-php80.rb
+++ Formula/vsh-php80.rb
@@ -2,7 +2,7 @@ class VshPhp80 < Formula
   desc "General-purpose scripting language"
   homepage "https://www.php.net/"
   url "https://www.php.net/distributions/php-8.0.0.tar.xz"
-  revision 72
+  revision 74
   license "PHP-3.01"
 
   bottle do
