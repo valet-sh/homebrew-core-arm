6acad4982b7cc1e4afdc6d4c8215fc974bfa4ef1
Philipp Dittert <p.dittert@techdivision.com>
2021-11-27 14:33:59 +0100

    update package source url

--- Formula/vsh-mariadb104.rb
+++ Formula/vsh-mariadb104.rb
@@ -1,10 +1,10 @@
 class VshMariadb104 < Formula
   desc "Drop-in replacement for MySQL"
   homepage "https://mariadb.org/"
-  url "http://ftp.hosteurope.de/mirror/mariadb.org/mariadb-10.4.21/source/mariadb-10.4.21.tar.gz"
+  url "https://archive.mariadb.org/mariadb-10.4.21/source/mariadb-10.4.21.tar.gz"
   sha256 "94dd2e6f5d286de8a7dccffe984015d4253a0568281c7440e772cfbe098a291d"
   license "GPL-2.0-only"
-  revision 6
+  revision 7
 
   bottle do
     root_url "https://github.com/valet-sh/homebrew-core/releases/download/bottles"
