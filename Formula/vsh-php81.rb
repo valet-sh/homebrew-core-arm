87903afe4b47880b8b66c29befe5888835b8d345
zelgerj <zelgerj@users.noreply.github.com>
2022-05-03 02:41:37 +0000

    Trigger bottle rebuild

--- Formula/vsh-php81.rb
+++ Formula/vsh-php81.rb
@@ -5,7 +5,7 @@ class VshPhp81 < Formula
   mirror "https://fossies.org/linux/www/php-8.1.3.tar.xz"
   sha256 "5d65a11071b47669c17452fb336c290b67c101efb745c1dbe7525b5caf546ec6"
   license "PHP-3.01"
-  revision 21
+  revision 22
 
   bottle do
     root_url "https://github.com/valet-sh/homebrew-core/releases/download/bottles"
