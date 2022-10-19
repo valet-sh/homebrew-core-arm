621e4b7072afdacdcef0949fdb5d3893fded258f
Philipp Dittert <p.dittert@techdivision.com>
2022-10-19 10:29:09 +0200

    bump version and add dependency

--- Formula/vsh-mariadb104.rb
+++ Formula/vsh-mariadb104.rb
@@ -1,10 +1,10 @@
 class VshMariadb104 < Formula
   desc "Drop-in replacement for MySQL"
   homepage "https://mariadb.org/"
-  url "https://archive.mariadb.org/mariadb-10.4.21/source/mariadb-10.4.21.tar.gz"
-  sha256 "94dd2e6f5d286de8a7dccffe984015d4253a0568281c7440e772cfbe098a291d"
+  url "https://downloads.mariadb.com/MariaDB/mariadb-10.4.22/source/mariadb-10.4.22.tar.gz"
+  sha256 "44bdc36eeb02888296e961718bae808f3faab268ed49160a785248db60500c00"
   license "GPL-2.0-only"
-  revision 27
+  revision 1
 
   bottle do
     root_url "https://github.com/valet-sh/homebrew-core/releases/download/bottles"
@@ -18,6 +18,7 @@ class VshMariadb104 < Formula
   depends_on "openssl@1.1"
   depends_on "pcre2"
   depends_on "lz4"
+  depends_on "lzo"
 
   uses_from_macos "bzip2"
   uses_from_macos "ncurses"
