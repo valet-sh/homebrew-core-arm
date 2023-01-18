ad7ebbcd0bca390489cd3a5b600de4704e9d4cd0
Philipp Dittert <p.dittert@techdivision.com>
2023-01-18 17:12:37 +0100

    add mariadb 10.6 formulae

---
+++ Formula/vsh-mariadb106.rb
@@ -0,0 +1,240 @@
+class VshMariadb106 < Formula
+  desc "Drop-in replacement for MySQL"
+  homepage "https://mariadb.org/"
+  url "https://downloads.mariadb.com/MariaDB/mariadb-10.6.11/source/mariadb-10.6.11.tar.gz"
+  sha256 "5784ba4c5d8793badba58348576824d9849ec152e9cbee47a1765161d840c94a"
+  license "GPL-2.0-only"
+  revision 1
+
+  bottle do
+    root_url "https://github.com/valet-sh/homebrew-core/releases/download/bottles"
+    sha256 big_sur: "9f53cabb2e7bb0e069768fd7f5bcec3ae1e831acfd31a1babb9b397161ccf693"
+  end
+
+  depends_on "bison" => :build
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "groonga"
+  depends_on "openssl@1.1"
+  depends_on "pcre2"
+
+  uses_from_macos "bzip2"
+  uses_from_macos "libxcrypt"
+  uses_from_macos "libxml2"
+  uses_from_macos "ncurses"
+  uses_from_macos "zlib"
+
+  fails_with gcc: "5"
+
+  def datadir
+    var/"#{name}"
+  end
+
+  def tmpconfdir
+    libexec/"config"
+  end
+
+  # fix compilation, remove in 10.6.12
+  patch do
+    url "https://github.com/mariadb-corporation/mariadb-connector-c/commit/44383e3df4896f2d04d9141f640934d3e74e04d7.patch?full_index=1"
+    sha256 "3641e17e29dc7c9bf24bc23e4d68da81f0d9f33b0568f8ff201c4ebc0487d26a"
+    directory "libmariadb"
+  end
+
+  def install
+    # Set basedir and ldata so that mysql_install_db can find the server
+    # without needing an explicit path to be set. This can still
+    # be overridden by calling --basedir= when calling.
+    inreplace "scripts/mysql_install_db.sh" do |s|
+      s.change_make_var! "basedir", "\"#{opt_libexec}\""
+      s.change_make_var! "ldata", "\"#{datadir}\""
+    end
+
+    # Use brew groonga
+    rm_r "storage/mroonga/vendor/groonga"
+
+    # -DINSTALL_* are relative to prefix
+    args = %W[
+      -DCMAKE_INSTALL_PREFIX=#{libexec}
+      -DMYSQL_DATADIR=#{datadir}
+      -DINSTALL_INCLUDEDIR=include/mysql
+      -DINSTALL_MANDIR=share/man
+      -DINSTALL_DOCDIR=share/doc/#{name}
+      -DINSTALL_INFODIR=share/info
+      -DINSTALL_MYSQLSHAREDIR=share/mysql
+      -DWITH_SSL=yes
+      -DWITH_UNIT_TESTS=OFF
+      -DDEFAULT_CHARSET=utf8mb4
+      -DDEFAULT_COLLATION=utf8mb4_general_ci
+      -DINSTALL_SYSCONFDIR=#{etc}/#{name}
+      -DCOMPILATION_COMMENT=valet.sh
+      -DPLUGIN_TOKUDB=NO
+    ]
+
+    system "cmake", ".", *std_cmake_args, *args
+
+    system "make"
+    system "make", "install"
+
+    # Save space
+    (libexec/"mysql-test").rmtree
+    (libexec/"sql-bench").rmtree
+
+    # Don't create databases inside of the prefix!
+    # See: https://github.com/Homebrew/homebrew/issues/4975
+    rm_rf prefix/"data"
+
+    # Link the setup script into bin
+    (libexec/"bin").install_symlink libexec/"scripts/mysql_install_db"
+
+    # Save space
+    #(prefix/"mysql-test").rmtree
+    #(prefix/"sql-bench").rmtree
+
+    # Link the setup script into bin
+    #bin.install_symlink prefix/"scripts/mysql_install_db"
+
+    # Fix up the control script and link into bin
+    #inreplace "#{prefix}/support-files/mysql.server", /^(PATH=".*)(")/, "\\1:#{HOMEBREW_PREFIX}/bin\\2"
+
+    #bin.install_symlink prefix/"support-files/mysql.server"
+
+    #libexec.install "bin", "docs", "include", "lib", "man", "share"
+
+    (bin/"mariadb10.6").write <<~EOS
+      #!/bin/bash
+      #{libexec}/bin/mysql --defaults-file=#{etc}/#{name}/my.cnf "$@"
+    EOS
+    (bin/"mariadump10.6").write <<~EOS
+      #!/bin/bash
+      #{libexec}/bin/mysqldump --defaults-file=#{etc}/#{name}/my.cnf "$@"
+    EOS
+    (bin/"mariaadmin10.6").write <<~EOS
+      #!/bin/bash
+      #{libexec}/bin/mysqladmin --defaults-file=#{etc}/#{name}/my.cnf "$@"
+    EOS
+
+    tmpconfdir.mkpath
+    (tmpconfdir/"my.cnf").write <<~EOS
+        !includedir #{etc}/#{name}/conf.d/
+    EOS
+    (tmpconfdir/"mysqld.cnf").write <<~EOS
+        [mysqld_safe]
+        socket =
+        nice		= 0
+
+        [mysqld]
+        socket =
+
+        bind-address		= 127.0.0.1
+        port		= 3319
+        basedir		= #{opt_libexec}
+        datadir		= #{datadir}
+        tmpdir		= /tmp
+        lc-messages-dir	= #{opt_libexec}/share
+        skip-external-locking
+
+        key_buffer_size		= 16M
+        max_allowed_packet	= 16M
+        thread_stack		= 192K
+        thread_cache_size       = 8
+        myisam-recover-options  = BACKUP
+        max_connections        = 200
+        log_error = #{var}/log/#{name}/error.log
+        max_binlog_size   = 100M
+
+        character-set-server=utf8mb4
+        collation-server=utf8mb4_general_ci
+    EOS
+    (tmpconfdir/"mysqldump.cnf").write <<~EOS
+        [mysqldump]
+        user = root
+        protocol=tcp
+        port=3319
+        host=127.0.0.1
+        quick
+        quote-names
+        max_allowed_packet	= 16M
+        default-character-set = utf8mb4
+    EOS
+
+    # move config files into etc
+    rm_rf etc/"#{name}/my.cnf"
+    (etc/"#{name}").install tmpconfdir/"my.cnf"
+    (etc/"#{name}/conf.d").install tmpconfdir/"mysqld.cnf"
+    (etc/"#{name}/conf.d").install tmpconfdir/"mysqldump.cnf"
+
+  end
+
+  def post_install
+    (var/"log/#{name}").mkpath
+    # make sure the datadir exists
+    datadir.mkpath
+
+        # Don't initialize database, it clashes when testing other MySQL-like implementations.
+    return if ENV["HOMEBREW_GITHUB_ACTIONS"]
+
+    unless File.exist? "#{datadir}/mysql/user.frm"
+      ENV["TMPDIR"] = nil
+      system libexec/"bin/mysql_install_db", "--verbose", "--auth-root-authentication-method=normal", "--user=#{ENV["USER"]}",
+        "--basedir=#{libexec}", "--datadir=#{datadir}", "--tmpdir=/tmp"
+    end
+  end
+
+  def caveats
+    s = <<~EOS
+      MariaDB 10.6 is configured to only allow connections from 127.0.0.1 on port 3319 by default
+
+      To connect run:
+          mariadb10.6 -uroot
+    EOS
+    s
+  end
+
+  plist_options :manual => "vsh-mariadb106"
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <true/>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{libexec}/bin/mysqld_safe</string>
+          <string>--defaults-file=#{etc}/#{name}/my.cnf</string>
+          <string>--datadir=#{datadir}</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>WorkingDirectory</key>
+        <string>#{datadir}</string>
+      </dict>
+      </plist>
+    EOS
+  end
+
+  test do
+    # expects datadir to be a completely clean dir, which testpath isn't.
+    dir = Dir.mktmpdir
+    system libexec/"bin/mysqld", "--initialize-insecure", "--user=#{ENV["USER"]}",
+    "--basedir=#{prefix}", "--datadir=#{dir}", "--tmpdir=#{dir}"
+
+    port = free_port
+    pid = fork do
+      exec libexec/"bin/mysqld", "--bind-address=127.0.0.1", "--datadir=#{dir}", "--port=#{port}"
+    end
+    sleep 2
+
+    output = shell_output("curl 127.0.0.1:#{port}")
+    output.force_encoding("ASCII-8BIT") if output.respond_to?(:force_encoding)
+    assert_match version.to_s, output
+  ensure
+    Process.kill(9, pid)
+    Process.wait(pid)
+  end
+end
