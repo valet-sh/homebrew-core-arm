2f0ec2908db84ddc88ce33f276116a5991ec3a52
Philipp Dittert <p.dittert@techdivision.com>
2023-02-03 12:05:42 +0100

    dump xdebug revision

--- Formula/vsh-php81.rb
+++ Formula/vsh-php81.rb
@@ -5,7 +5,7 @@ class VshPhp81 < Formula
   mirror "https://fossies.org/linux/www/php-8.1.14.tar.xz"
   sha256 "e16e47a872d58685913ac848ce92ec49f42c1828110c98c65fb6265a08724a1a"
   license "PHP-3.01"
-  revision 8
+  revision 9
 
   bottle do
     root_url "https://github.com/valet-sh/homebrew-core/releases/download/bottles"
@@ -59,8 +59,8 @@ class VshPhp81 < Formula
   patch :DATA
 
   resource "xdebug_module" do
-    url "https://github.com/xdebug/xdebug/archive/refs/tags/3.1.1.tar.gz"
-    sha256 "f8d46e0127b4a7c7d392f0ee966233bf5cfd1ade7364cc807fe5397c7de0579a"
+    url "https://github.com/xdebug/xdebug/archive/refs/tags/3.2.0.tar.gz"
+    sha256 "a5979f2060b92375523662f451bfebd76b718116921c60bcdf8e87be0c58dd72"
   end
 
   resource "imagick_module" do
