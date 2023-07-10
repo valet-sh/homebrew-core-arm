227d532c160c102b14390de39b70c8ca536ae6df
CI <ci@valet.sh>
2023-07-10 08:24:12 +0000

    [skip ci] Update bottle information in formulae ***NO_CI***

--- Formula/vsh-php82.rb
+++ Formula/vsh-php82.rb
@@ -9,7 +9,7 @@ class VshPhp82 < Formula
 
   bottle do
     root_url "https://github.com/valet-sh/homebrew-core/releases/download/bottles"
-    sha256 big_sur: "d3690e279fc91c02afab1b38b1ab92fdafe94f74c3870a19d5ace0721b1b50a2"
+    sha256 big_sur: "f2ab5730ae1f8f1dd0633aaad07d0571dc8b7babd2cc7f518ea48ee933dfdab5"
   end
 
   depends_on "pkg-config" => :build
