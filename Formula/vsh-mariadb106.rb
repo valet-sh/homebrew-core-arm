4d427876c65f40ef80645644ccf745840b36f9fd
Philipp Dittert <p.dittert@techdivision.com>
2023-11-09 08:34:38 +0100

    bump revision

--- Formula/vsh-mariadb106.rb
+++ Formula/vsh-mariadb106.rb
@@ -4,7 +4,7 @@ class VshMariadb106 < Formula
   url "https://downloads.mariadb.com/MariaDB/mariadb-10.6.11/source/mariadb-10.6.11.tar.gz"
   sha256 "5784ba4c5d8793badba58348576824d9849ec152e9cbee47a1765161d840c94a"
   license "GPL-2.0-only"
-  revision 13
+  revision 14
 
   bottle do
     root_url "https://github.com/valet-sh/homebrew-core/releases/download/bottles"
