5164317c12d9a44f94aeb836be0def57288838cc
zelgerj <zelgerj@users.noreply.github.com>
2023-12-18 06:39:24 +0000

    Trigger bottle rebuild

--- Formula/vsh-mysql57.rb
+++ Formula/vsh-mysql57.rb
@@ -5,7 +5,7 @@ class VshMysql57 < Formula
   url "https://cdn.mysql.com/Downloads/MySQL-5.7/mysql-boost-5.7.44.tar.gz"
   sha256 "b8fe262c4679cb7bbc379a3f1addc723844db168628ce2acf78d33906849e491"
   license "GPL-2.0-only"
-  revision 1
+  revision 2
 
   bottle do
     root_url "https://github.com/valet-sh/homebrew-core/releases/download/bottles"
