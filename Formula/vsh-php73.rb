284ae2abc98d617e547b2fccba66551bbd738d46
Philipp Dittert <p.dittert@techdivision.com>
2024-01-08 08:09:17 +0100

    bump php revisions

--- Formula/vsh-php73.rb
+++ Formula/vsh-php73.rb
@@ -5,7 +5,7 @@ class VshPhp73 < Formula
   mirror "https://fossies.org/linux/www/php-7.3.33.tar.xz"
   sha256 "166eaccde933381da9516a2b70ad0f447d7cec4b603d07b9a916032b215b90cc"
   license "PHP-3.01"
-  revision 181
+  revision 182
 
   bottle do
     root_url "https://github.com/valet-sh/homebrew-core/releases/download/bottles"
