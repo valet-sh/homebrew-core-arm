7743eae37039db132736801d73ec8a1a7e435ee7
zelgerj <zelgerj@users.noreply.github.com>
2024-03-22 01:31:31 +0000

    Trigger bottle rebuild

--- Formula/vsh-mysql80.rb
+++ Formula/vsh-mysql80.rb
@@ -4,7 +4,7 @@ class VshMysql80 < Formula
   homepage "https://dev.mysql.com/doc/refman/8.0/en/"
   url "https://cdn.mysql.com/Downloads/MySQL-8.0/mysql-boost-8.0.34.tar.gz"
   sha256 "0b881a19bcef732cd4dbbfc8dfeb84eff61f5dfe0d9788d015d699733e0adf1f"
-  revision 11
+  revision 12
   license "GPL-2.0"
 
   bottle do
