dea69cae913b7e6a082dab0095068440216ceb8f
Philipp Dittert <p.dittert@techdivision.com>
2025-11-10 09:59:09 +0100

    update

--- Formula/vsh-php81.rb
+++ Formula/vsh-php81.rb
@@ -247,9 +247,9 @@ class VshPhp81 < Formula
       touch var/"log/php-fpm#{bin_suffix}.log"
     end
 
-    mv "#{bin}/pecl", "#{bin}/pecl#{bin_suffix}"
-    mv "#{bin}/pear", "#{bin}/pear#{bin_suffix}"
-    mv "#{bin}/peardev", "#{bin}/peardev#{bin_suffix}"
+    #mv "#{bin}/pecl", "#{bin}/pecl#{bin_suffix}"
+    #mv "#{bin}/pear", "#{bin}/pear#{bin_suffix}"
+    #mv "#{bin}/peardev", "#{bin}/peardev#{bin_suffix}"
 
   end
 
